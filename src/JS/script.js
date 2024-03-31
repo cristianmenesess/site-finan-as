bb9e94dd46972867445bd9be7c65fa8842f66736
Cristian Oliveira <cristianoliveira6699@gmail.com>
2024-03-31 17:17:49 -0300

    v2

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,16 +1,27 @@
 // Selecionando os elementos do DOM
 const balanceElement = document.getElementById('current-balance');
+const incomeValueElement = document.getElementById('income-value');
+const expenseValueElement = document.getElementById('expense-value');
 const transactionAmountInput = document.getElementById('transaction-amount');
 const addTransactionButton = document.getElementById('add-transaction');
 const subtractTransactionButton = document.getElementById('subtract-transaction');
 const transactionListElement = document.getElementById('transaction-list');
 
-// Inicializando o saldo atual
+// Inicializando o saldo atual, entrada e saída
 let currentBalance = 0;
+let currentIncome = 0;
+let currentExpense = 0;
 
 // Função para atualizar o saldo exibido
 function updateBalance() {
   balanceElement.textContent = `R$ ${currentBalance.toFixed(2)}`;
+}
+
+// Função para atualizar os valores de entrada, saída e saldo atual
+function updateBalanceValues() {
+  incomeValueElement.textContent = `R$ +${currentIncome.toFixed(2)}`;
+  expenseValueElement.textContent = `R$ -${currentExpense.toFixed(2)}`;
+  updateBalance();
 }
 
 // Função para adicionar uma transação
@@ -19,14 +30,18 @@ function addTransaction(amount, type) {
   const transactionText = `${type === 'addition' ? '+' : '-'} R$ ${amount.toFixed(2)}`;
   transactionItem.textContent = transactionText;
 
-  // Adiciona a classe correta com base no tipo de transação
   if (type === 'addition') {
     transactionItem.classList.add('addition');
+    currentIncome += amount;
+    currentBalance += amount; // Atualiza o saldo atual para transações de adição
   } else {
     transactionItem.classList.add('subtraction');
+    currentExpense += amount;
+    currentBalance -= amount; // Atualiza o saldo atual para transações de subtração
   }
 
   transactionListElement.appendChild(transactionItem);
+  updateBalanceValues();
 }
 
 // Função para adicionar uma transação de adição
@@ -38,8 +53,6 @@ function handleAddTransaction() {
     return;
   }
 
-  currentBalance += transactionAmount;
-  updateBalance();
   addTransaction(transactionAmount, 'addition');
 
   transactionAmountInput.value = '';
@@ -54,8 +67,6 @@ function handleSubtractTransaction() {
     return;
   }
 
-  currentBalance -= transactionAmount;
-  updateBalance();
   addTransaction(transactionAmount, 'subtraction');
 
   transactionAmountInput.value = '';
@@ -65,5 +76,35 @@ function handleSubtractTransaction() {
 addTransactionButton.addEventListener('click', handleAddTransaction);
 subtractTransactionButton.addEventListener('click', handleSubtractTransaction);
 
-// Inicializando o saldo exibido
-updateBalance();
+// Inicializando os valores exibidos
+updateBalanceValues();
+
+//Esconder Saldo Total
+// Selecionando o elemento de saldo total e o ícone de olhinho
+const currentBalanceElement = document.getElementById('current-balance');
+const toggleBalanceIcon = document.querySelector('.toggle-balance i');
+
+// Armazenar o valor original do saldo total
+let originalBalanceValue = currentBalanceElement.textContent;
+
+// Função para alternar a visibilidade do saldo total
+function toggleBalanceVisibility() {
+  if (currentBalanceElement.textContent === 'R$ 0,00') {
+    return; // Não esconde o saldo se for zero
+  }
+
+  if (currentBalanceElement.textContent === '***') {
+    currentBalanceElement.textContent = originalBalanceValue;
+    toggleBalanceIcon.classList.remove('fa-eye-slash');
+    toggleBalanceIcon.classList.add('fa-eye');
+  } else {
+    originalBalanceValue = currentBalanceElement.textContent;
+    currentBalanceElement.textContent = '***';
+    toggleBalanceIcon.classList.remove('fa-eye');
+    toggleBalanceIcon.classList.add('fa-eye-slash');
+  }
+}
+
+// Adicionando event listener ao ícone de olhinho
+toggleBalanceIcon.addEventListener('click', toggleBalanceVisibility);
+
